cb04e98a3a932e5e81979d828b5b366bce76a3ae
HungChuanYu <ken.hung@bluextrade.com>
2021-02-08 15:16:00 +0800

    pytest practice

---
+++ src/models/train.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def split_into_training_and_testing_sets(data_array):
+    dim = data_array.ndim
+    if dim != 2:
+        raise ValueError(
+            "Argument data_array must be two dimensional. Got {0} dimensional array instead!".format(dim))
+    num_rows = data_array.shape[0]
+    if num_rows < 2:
+        raise ValueError(
+            "Argument data_array must have at least 2 rows, it actually has just {0}".format(num_rows))
+    # Fill in with the correct float
+    '''
+    The function randomly splits the argument array into training and testing arrays in the ratio 3:1, and returns the resulting arrays in a tuple.
+    '''
+    num_training = int(float(3/4) * data_array.shape[0])
+    permuted_indices = np.random.permutation(data_array.shape[0])
+    return data_array[permuted_indices[:num_training], :], data_array[permuted_indices[num_training:], :]
+
+
+def model_test():
+    pass
---
+++ tests/models/test_train.py
@@ -0,0 +1,55 @@
+import pytest
+import numpy as np
+
+## handling the path
+import os
+import sys
+src_models_train_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+sys.path.append(src_models_train_path)
+print(sys.path)
+
+
+from src.models.train import split_into_training_and_testing_sets
+
+# Declare the test class
+class TestSplitIntoTrainingAndTestingSets(object):
+    # Fill in with the correct mandatory argument
+    def test_on_one_row(self):
+        test_argument = np.array([[1382.0, 390167.0]])
+        with pytest.raises(ValueError) as exc_info:
+            split_into_training_and_testing_sets(test_argument)
+        expected_error_msg = "Argument data_array must have at least 2 rows, it actually has just 1"
+        assert exc_info.match(expected_error_msg)
+
+    def test_on_six_rows(self):
+        test_input = np.array([[2081.0, 314942.0],
+                               [1059.0, 186606.0],
+                               [1148.0, 206186.0],
+                               [1506.0, 248419.0],
+                               [1210.0, 214114.0],
+                               [1697.0, 277794.0],
+                               ]
+                              )
+        expected_length_training_set = 4
+        expected_length_testing_set = 2
+        actual = split_into_training_and_testing_sets(test_input)
+        assert actual[0].shape[0] == expected_length_training_set, \
+            "The actual number of rows in the training array is not 4"
+        assert actual[1].shape[0] == expected_length_testing_set, \
+            "The actual number of rows in the testing array is not 2"
+
+# Add a reason for the expected failure
+@pytest.mark.xfail(reason="Using TDD, model_test() has not yet been implemented")
+class TestModelTest(object):
+    def test_on_linear_data(self):
+        test_input = np.array([[1.0, 3.0], [2.0, 5.0], [3.0, 7.0]])
+        expected = 1.0
+        actual = model_test(test_input, 2.0, 1.0)
+        message = "model_test({0}) should return {1}, but it actually returned {2}".format(
+            test_input, expected, actual)
+        assert actual == pytest.approx(expected), message
+
+    def test_on_one_dimensional_array(self):
+        test_input = np.array([1.0, 2.0, 3.0, 4.0])
+        with pytest.raises(ValueError) as exc_info:
+            model_test(test_input, 1.0, 1.0)
